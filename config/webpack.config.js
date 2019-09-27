c4488363f1c9eba5d69a102783e3329eb0f9ad35
Jett Wu <xrowuje@sickcn.net>
2019-09-26 22:44:05 -0400

    add file-loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,8 +1,15 @@
+const path = require('path');
 const HtmlWebPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+
 module.exports = {
     entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js'
+        // publicPath: '/dist'
+    },
     module: {
         rules: [
             {
@@ -12,15 +19,26 @@ module.exports = {
                     loader: 'babel-loader'
                 }
             }, {
-                test: /\.s?css/i,
+                test: /\.s?css$/i,
                 exclude: /(node_modules|dist)/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }, {
-                test: /\.html$/,
+                test: /\.html/i,
+                exclude: /(node_modules|dist)/i,
                 use: {
                     loader: 'html-loader',
                     options: {
-                        minimize: true
+                        minimize: false
+                    }
+                }
+            }, {
+                test: /\.(jpg|png)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        publicPath: 'img/'
                     }
                 }
             }
@@ -29,7 +47,7 @@ module.exports = {
     plugins: [
         new HtmlWebPlugin({
             template: './src/index.html',
-            filename: './index.html'
+            filename: 'index.html'
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
