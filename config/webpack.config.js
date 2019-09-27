d3ae54bed308467fe57d8f814824d088b0edeb51
Jett Wu <xrowuje@sickcn.net>
2019-09-27 00:37:04 -0400

    minor change

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebPlugin = require('html-webpack-plugin');
+const HtmlWepbackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -14,18 +14,16 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/i,
+                test: /\.jsx?$/,
                 exclude: /(node_modules|dist)/i,
                 use: {
                     loader: 'babel-loader'
                 }
             }, {
-                test: /\.s?css$/i,
-                exclude: /(node_modules|dist)/i,
+                test: /\.s?css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }, {
-                test: /\.html/i,
-                exclude: /(node_modules|dist)/i,
+                test: /\.html$/i,
                 use: {
                     loader: 'html-loader',
                     options: {
@@ -33,7 +31,7 @@ module.exports = {
                     }
                 }
             }, {
-                test: /\.(jpg|png)$/i,
+                test: /\.(jpg|png)$/,
                 use: {
                     loader: 'file-loader',
                     options: {
@@ -46,7 +44,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebPlugin({
+        new HtmlWepbackPlugin({
             template: './src/index.html',
             filename: 'index.html'
         }),
