307672a3e65c85dcf55c76c29ecfc119b1f0dce8
Jettwu <jettwu1130@gmail.com>
2019-10-17 00:37:46 -0400

    implement upload img file function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import './style/main.scss';
 
-const el = document.createElement('div')
+var MAXIMGSIZE = 2100000; //上传图片最大值(单位字节)（ 2 M = 2097152 B ）超过2M上传失败
+const inputImage = document.querySelector('#uploadImage')
 
-el.innerHTML = 'Hello world!';
+inputImage.onchange = function (e) {
+  // console.log(e.target === this) true
+  if (this.files[0].size > MAXIMGSIZE) {
+    alert('File size too big. Max 2M')
+    this.value = ''
+  } else {
+    const reader = new FileReader()
 
-document.body.appendChild(el);
+    reader.onload = function (e) {
+      const img = document.createElement('img')
+      img.src = reader.result // event.target.result <-- same -->
+      img.setAttribute('style', 'height: 10vh')
+      img.onclick = function (e) {
+        inputImage.value = ''
+        this.parentNode.removeChild(this)
+      }
+      document.querySelector('body').appendChild(img)
+    }
+
+    reader.readAsDataURL(this.files[0])
+  }
+}
+
